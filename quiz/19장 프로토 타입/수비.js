97224e415f8a31dc57f7323fa2e3c0f5a5bd9500
Jeong Subi <jsb97613@naver.com>
2022-09-06 18:57:31 +0900

    Update 수비.js

--- quiz/19장 프로토 타입/수비.js
+++ quiz/19장 프로토 타입/수비.js
@@ -18,16 +18,16 @@ curry.sayDelicious = function() {
 }
 
 
-```
-// 1. (1), (2)번 중 어떤 메서드가 호출되는가?
+/*
+1. (1), (2)번 중 어떤 메서드가 호출되는가?
 curry.sayDelicious(); 
 
-// 2. (1), (2)번 중 어떤 메서드가 호출되는가?
+2. (1), (2)번 중 어떤 메서드가 호출되는가?
 delete curry.sayDelicious; 
 curry.sayDelicious();    
 
 
-// 3. 한번더 아래의 코드를 실행했을때, (1), (2)번 중 어떤 메서드가 호출되는가?, 그리고 그 이유는?
+3. 한번더 아래의 코드를 실행했을때, (1), (2)번 중 어떤 메서드가 호출되는가?, 그리고 그 이유는?
 delete curry.sayDelicious; 
 curry.sayDelicious();   
-```
+*/
